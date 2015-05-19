475734cb77850d32a82f2ce73a27c3138eb0f30b
Refat <sniperefat@gmail.com>
2015-05-19 17:32:24 +0600

    Portfoilo Detail Button added

--- js/controller/Portfolio.js
+++ js/controller/Portfolio.js
@@ -1,3 +1,17 @@
 app.controller('portfolioCtrl',function($scope){
 
+	 $scope.project = [{name:'First App',details:"The project is done with the latest technologies. I have used angularjs for front end and Sailsjs for backend.", image:['image/2.jpg','image/3.jpg','image/4.jpg']},
+	{name:'Second App',details:"The project is done with the latest technologies. I have used angularjs for front end and Sailsjs for backend.", image:['image/3.jpg','image/4.jpg','image/5.jpg']},
+	{name:'Third App',details:"The project is done with the latest technologies. I have used angularjs for front end and Sailsjs for backend.", image:['image/4.jpg','image/5.jpg','image/6.jpg']},
+	{name:'Fourth App',details:"The project is done with the latest technologies. I have used angularjs for front end and Sailsjs for backend.", image:['image/5.jpg','image/6.jpg','image/1.jpg']},
+	{name:'Fifth App',details:"The project is done with the latest technologies. I have used angularjs for front end and Sailsjs for backend.", image:['image/6.jpg','image/1.jpg','image/2.jpg']}
+	];
+
+	$scope.selected = 0;
+
+    $scope.showModal = false;
+    $scope.toggleModal = function(pos){
+    	$scope.selected=pos;
+        $scope.showModal = !$scope.showModal;
+    };
 })
